75fbf1c23b473d099a911f7d9af504380fb1357f
Fred Atherden <43879983+fred-atherden@users.noreply.github.com>
2025-11-24 19:41:59 +0000

    whitespace formatting

--- __tests__/pdf.test.js
+++ __tests__/pdf.test.js
@@ -17,11 +17,12 @@ const TEST_CASES = [
 ];
 
 function normalizeHtml(html) {
-    // remove data-..., id, and empty class attributes
-    let cleaned = html.replace(/\sdata-\w+=\".*?\"/g, '');
-    cleaned = cleaned.replace(/\sid=\".*?\"/g, '');
-    cleaned = cleaned.replace(/\sclass=\"\"/g, '');
-    return pretty(cleaned).trim();
+  // remove data-..., id, and empty class attributes
+  let cleaned = html.replace(/\sdata-\w+=\".*?\"/g, '');
+  cleaned = html.replace(/\sdata-\w+=\".*?\"/g, '');
+  cleaned = cleaned.replace(/\sid=\".*?\"/g, '');
+  cleaned = cleaned.replace(/\sclass=\"\"/g, '');
+  return pretty(cleaned).trim();
 };
 
 expect.extend({
@@ -51,37 +52,37 @@ describe('PDF generation tests (pagedjs HTML output)', () => {
   test.each(TEST_CASES)(
     'generated PDF-ready HTML for $id (exhibiting $description) should match expected PDF-ready HTML',
     async ({ id }) => {
-        const inputFilePath = path.join(TEST_CASE_DIR, `${id}.expected.html`);
-        const expectedHtmlFile = path.join(TEST_CASE_DIR, `${id}.pdf.expected.html`);
+      const inputFilePath = path.join(TEST_CASE_DIR, `${id}.expected.html`);
+      const expectedHtmlFile = path.join(TEST_CASE_DIR, `${id}.pdf.expected.html`);
 
-        if (!fs.existsSync(inputFilePath)) throw new Error(`Missing input HTML file: ${inputFilePath}`);
-        if (!fs.existsSync(expectedHtmlFile)) throw new Error(`Missing expected HTML file: ${expectedHtmlFile}`);
+      if (!fs.existsSync(inputFilePath)) throw new Error(`Missing input HTML file: ${inputFilePath}`);
+      if (!fs.existsSync(expectedHtmlFile)) throw new Error(`Missing expected HTML file: ${expectedHtmlFile}`);
 
-        // For pagedjs to load the styles, this needs to be copied into the root/paged-js folder(!)
-        const tempInputFile = path.join(PROJECT_ROOT, 'paged-js', `${id}-temp.expected.html`);
-        fs.copyFileSync(inputFilePath, tempInputFile);
-        
-        const expectedHtml = fs.readFileSync(expectedHtmlFile, 'utf-8');
-        const actualHtmlPath = path.join(TEST_CASE_DIR, `${id}.pdf.actual.html`);
-        
-        await generatePDF(tempInputFile, actualHtmlPath, true);
-        
-        if (!fs.existsSync(actualHtmlPath)) throw new Error(`Error generating PDF-ready HTML file: ${actualHtmlPath}`);
-        const actualHtml = fs.readFileSync(actualHtmlPath, 'utf-8');
+      // For pagedjs to load the styles, this needs to be copied into the root/paged-js folder(!)
+      const tempInputFile = path.join(PROJECT_ROOT, 'paged-js', `${id}-temp.expected.html`);
+      fs.copyFileSync(inputFilePath, tempInputFile);
 
+      const expectedHtml = fs.readFileSync(expectedHtmlFile, 'utf-8');
+      const actualHtmlPath = path.join(TEST_CASE_DIR, `${id}.pdf.actual.html`);
+
+      await generatePDF(tempInputFile, actualHtmlPath, true);
+
+      if (!fs.existsSync(actualHtmlPath)) throw new Error(`Error generating PDF-ready HTML file: ${actualHtmlPath}`);
+      const actualHtml = fs.readFileSync(actualHtmlPath, 'utf-8');
+
+      try {
+        expect(actualHtml).toMatchHtml(expectedHtml);
         try {
-            expect(actualHtml).toMatchHtml(expectedHtml);
-            try {
-              fs.unlinkSync(actualHtmlPath)
-            } catch (err) {}
-        } catch (err) {
-            console.log(`Test failed. Generated file located at: ${actualHtmlPath}`);
-            throw err;
-        } finally {
-            try {
-                fs.unlinkSync(tempInputFile)
-            } catch (err) {}
-        }
+          fs.unlinkSync(actualHtmlPath)
+        } catch (err) { }
+      } catch (err) {
+        console.log(`Test failed. Generated file located at: ${actualHtmlPath}`);
+        throw err;
+      } finally {
+        try {
+          fs.unlinkSync(tempInputFile)
+        } catch (err) { }
+      }
     },
     TIMEOUT
   );
